97d1e320246d99d3c6e43a798111758d00539008
macaron924 <akarin915mor@gmail.com>
2025-02-09 17:26:14 +0900

    サイト名修正

--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -1,4 +1,4 @@
 <footer class="p-2 text-center bg-[#eee]">
-    <h3 class="text-xl">HiMiTsuDB</h3>
+    <h3 class="text-xl">SecretCloset</h3>
     <p class="text-xs">このサイトでは、アイコンの一部に<a href="https://pictogrammers.com/library/mdi/">Material Design Icons</a>を使用しています。</p>
 </footer>
