b94dd986978de38cce1507bba98bd501662e59da
macaron924 <akarin915mor@gmail.com>
2025-02-23 21:34:34 +0900

    トップページ文字サイズ調整

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <main class="grow p-2.5">
     <div class="grid m-auto w-6/10 place-content-center place-items-center text-center grid-cols-1 md:grid-cols-2">
-        <a href="./card/" class="m-2.5 p-2.5 w-9/10 aspect-square border-5 border-[#ccc] rounded-2xl bg-white text-2xl"><span class="mdi--cards !w-9/10 !h-9/10 text-[rgb(255,150,255)]"></span><br>アイプリカード一覧</a>
-        <a href="./item/" class="m-2.5 p-2.5 w-9/10 aspect-square border-5 border-[#ccc] rounded-2xl bg-white text-2xl"><span class="mdi--clothes-hanger !w-9/10 !h-9/10 text-[rgb(150,255,255)]"></span><br>コーデアイテム一覧</a>
-        <a href="./setting/" class="col-span-full m-2.5 p-2.5 w-90/100 md:w-95/100 border-5 border-[#ccc] rounded-2xl bg-white text-2xl">データ管理</a>
+        <a href="./card/" class="m-2.5 p-2.5 w-9/10 aspect-square border-5 border-[#ccc] rounded-2xl bg-white text-xl"><span class="mdi--cards !w-9/10 !h-9/10 text-[rgb(255,150,255)]"></span><br>アイプリカード一覧</a>
+        <a href="./item/" class="m-2.5 p-2.5 w-9/10 aspect-square border-5 border-[#ccc] rounded-2xl bg-white text-xl"><span class="mdi--clothes-hanger !w-9/10 !h-9/10 text-[rgb(150,255,255)]"></span><br>コーデアイテム一覧</a>
+        <a href="./setting/" class="col-span-full m-2.5 p-2.5 w-90/100 md:w-95/100 border-5 border-[#ccc] rounded-2xl bg-white text-xl">データ管理</a>
     </div>
 </main>
