7344fcfc9aa14469e012c037093c4dfee307c8c7
macaron924 <akarin915mor@gmail.com>
2025-03-07 11:38:22 +0900

    UI調整

--- src/routes/parts/+page.svelte
+++ src/routes/parts/+page.svelte
@@ -19,12 +19,20 @@
             <div class="text-2xl">{title.title}</div>
                 {#each title.groups as group}
                     <div>
-                        <div class="pt-1 text-xl">・{group.group}</div>
-                        <div class="flex flex-row flex-wrap">
+                        <div class="pt-1 text-xl">・{group.group}
+                            {#if isDream}
+                                <div class="inline-block align-bottom ml-2 overflow-hidden border-1 border-[#ccc] rounded-xl">
+                                    <button aria-label="ランク+1" class="inline-block bg-[#eee] w-5"><span class="mdi--plus align-middle"></span></button>
+                                    <input type="text" class="inline-block w-10" disabled>
+                                    <button aria-label="ランク-1" class="inline-block bg-[#eee] w-5"><span class="mdi--minus align-middle"></span></button>
+                                </div>
+                            {/if}
+                        </div>
+                        <div class="grid grid-cols-3 md:grid-cols-6">
                             {#each group.parts as parts}
                             {@const data = getPartsData(parts)}
-                                <div class="m-1 p-1 w-40 text-center border-1 border-[#aaa] rounded-xl">
-                                    <img src="{base}/img/parts/{data?.img}_150.webp" alt="{parts}" class="m-auto">
+                                <div class="m-1 p-1 w-9/10 text-center border-1 border-[#aaa] rounded-xl">
+                                    <img src="{base}/img/parts/{data?.img}_150.webp" alt="{parts}" class="m-auto w-full max-w-40">
                                     <span class="text-wrap">{data?.get}</span>
                                 </div>
                             {/each}
