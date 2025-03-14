5ca2bfe96f1e9ab96d53961bef119ffcee7c7e87
macaron924 <akarin915mor@gmail.com>
2025-03-14 11:42:13 +0900

    修正

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -3,4 +3,4 @@ import type { CardInventory, ItemInventory, RankList } from "./types";
 
 export const cardInventoryStore = persisted("cardPosData", {} as CardInventory);
 export const itemInventoryStore = persisted("itemPosData", {} as ItemInventory);
-export const rankListStore = persisted("itemPosData", {} as RankList);
+export const rankListStore = persisted("rankListData", {} as RankList);
