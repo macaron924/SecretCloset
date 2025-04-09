c0ea1ff2827fe56908b9bd1ffd70a8df81ddd17e
macaron924 <akarin915mor@gmail.com>
2025-04-09 22:46:29 +0900

    プリマジコーデ一覧追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,6 @@
         <a href="./item/" class="m-2.5 p-2.5 w-9/10 aspect-square border-5 border-[#ccc] rounded-2xl bg-white text-xl"><span class="mdi--clothes-hanger !w-9/10 !h-9/10 text-[rgb(150,255,255)]"></span><br>コーデアイテム一覧</a>
         <a href="./setting/" class="col-span-full m-2.5 p-2.5 w-90/100 md:w-95/100 border-5 border-[#ccc] rounded-2xl bg-white text-xl">データ管理</a>
         <a href="./parts/" class="col-span-full m-2.5 p-2.5 w-90/100 md:w-95/100 border-5 border-[#ccc] rounded-2xl bg-white text-xl">マイキャラパーツ</a>
+        <a href="./pm/" class="col-span-full m-2.5 p-2.5 w-90/100 md:w-95/100 border-5 border-[#ccc] rounded-2xl bg-white text-xl">プリマジコーデ一覧</a>
     </div>
 </main>
---
+++ src/routes/pm/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+    import pmItems from "$lib/assets/pm_item.json";
+    import pmVerse from "$lib/assets/pm_verse.json";
+</script>
+<main class="grow mt-15 p-2.5">
+    <div class="flex flex-wrap">
+        {#each pmItems as pmItem}
+            <div class="m-1 p-1 w-30 bg-white/70 text-center">
+                <span>{pmItem.sealId}</span>
+                <img
+                    src="{base}/img/pm/{pmItem.id}_150.webp"
+                    alt="{pmItem.coordinationName}"
+                    class={{
+                        "w-full aspect-square opacity-100": true,
+                        "!opacity-30": !pmVerse.includes(pmItem.sealId)
+                    }}>
+                <span>{pmItem.coordinationName}</span>
+            </div>
+        {/each}
+    </div>
+</main>
